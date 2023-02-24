08c0c5279bcfa1878aaa0bb548344c354593a4e9
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-02-24 11:08:46 +0000

    x

---
+++ js/study.js
@@ -0,0 +1,29 @@
+
+
+
+import * as THREE from '../node_modules/three/build/three.module.js';
+import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+
+
+class App {
+	constructor() {
+        Ammo().then(() => {
+            const overlappingPairCache = new Ammo.btDbvtBroadphase();
+            const collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
+            const dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration);
+            const solver = new Ammo.btSequentialImpulseConstraintSolver();
+
+            const physicsWorld = new Ammo.btDiscreteDynamicsWorld(
+                dispatcher, overlappingPairCache, solver, collisionConfiguration);
+            physicsWorld.setGravity(new Ammo.btVector3(0, -9.807, 0));
+
+            this._physicsWorld = physicsWorld;
+            console.log(Ammo.btCollisionObject)
+        })
+    }
+
+}
+
+window.onload = function () {
+	new App();
+};
